5722a24a7b2fe63ae4c0e4dda270881be8407538
cho-k <52272347+cho-k@users.noreply.github.com>
2021-08-20 07:10:47 +0900

    Update index.js

--- docs/crontab/index.js
+++ docs/crontab/index.js
@@ -1,8 +1,10 @@
 const mins = document.getElementById('mins');
+const everyMin = document.getElementById('every-min');
 
 for (let i=0; i<60; i++) {
   const option = document.createElement('option');
   option.text = i;
   option.value = i;
   mins.appendChild(option);
+  everyMin.appendChild(option);
 }
