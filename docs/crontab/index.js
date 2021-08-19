384b510bd2e510984b20ebd0d1ff2a8eabc58387
cho-k <52272347+cho-k@users.noreply.github.com>
2021-08-20 08:36:59 +0900

    Update index.js

--- docs/crontab/index.js
+++ docs/crontab/index.js
@@ -1,1 +1,19 @@
-
+window.addEventListener('DOMContentLoaded', () => {
+  const button = document.querySelector('button');
+  
+  button.addEventListener('click', () => {
+    const mins = document.getElementById('mins');
+    const everyMin = document.getElementById('every-min');
+    const hours = document.getElementById('hours');
+    const everyHour = document.getElementById('every-hour');
+    const days = document.getElementById('days');
+    const everyDay = document.getElementById('every-day');
+    const months = document.getElementById('months');
+    const everyMonth = document.getElementById('every-month');
+    const weeks = document.getElementById('weeks');
+    
+    for (let i = 0; i < mins.length; i++) {
+      console.log(mins[i].value);
+    }
+  });
+});
